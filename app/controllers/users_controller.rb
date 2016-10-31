972d9458d44e8a57fa60bc5e38928282dddca7b7
Cedric Wider <wider.cedric@gmail.com>
2016-10-31 22:10:52 +0100

    don't show passwords after registering a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @current_user = User.new(user_params)
 
     if @current_user.save
-      render json: @current_user, status: :created, location: @current_user
+      render json: user_response_object(@current_user), status: :created, location: @current_user
     else
       render json: @current_user.errors, status: :unprocessable_entity
     end
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user
     @user ||= User.find_by(email: params[:id])
   end
@@ -48,5 +49,16 @@ class UsersController < ApplicationController
     user_params = JSON.parse(request.body.read)
                     .symbolize_keys.slice(:first_name, :last_name, :hero_name, :email, :password)
     user_params.update(user_params) { |k, v| k == :password ? encrypt(v) : v }
+  end
+
+  def user_response_object(user)
+    {
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      hero_name: user.hero_name,
+      email: user.email,
+      clan_id: user.clan_id
+    }
   end
 end
